4be156a81741911ecfb2fafa8f5430548ac9de27
mdjahedahmed666 <lucommunity3@gmail.com>
2021-09-16 01:34:03 +0600

    second commit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-var app = new Vue({
+const app = new Vue({
   el: "#app",
   data: {
+    jahedAge: 23,
+    tanvirAge: 22,
     message: "Hello Jahed in Vue",
     message2: "Thank you for entering vue world",
     seen: true,
@@ -16,5 +18,21 @@ var app = new Vue({
     reverseMessage() {
       this.message3 = this.message3.split("").reverse().join("");
     },
+  },
+});
+
+new Vue({
+  data: {
+    a: 1,
+  },
+  created: function () {
+    console.log("a is: " + this.a); // => "a is: 1"
+  },
+  mounted: function () {
+    this.$nextTick(function () {
+      // Code that will run only after the
+      // entire view has been rendered
+      console.log("hello");
+    });
   },
 });
